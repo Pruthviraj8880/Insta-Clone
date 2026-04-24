a33cd11ff01d1accdf0aa35f6d125cf3fc8f6a75
Pruthviraj Vala <pruthvirajvala@Pruthvirajs-MacBook-Pro.local>
2026-04-24 21:17:48 +0530

    data added

---
+++ data.js
@@ -0,0 +1,52 @@
+export const POSTS = [
+  {
+    id: 1,
+    username: "react_learner",
+    profilePic: "https://i.pravatar.cc/150?u=1",
+    postImage: "https://picsum.photos/id/1015/600/600",
+    likes: 420,
+    caption: "Building my first React App! 🚀 #coding #webdev",
+  },
+  {
+    id: 2,
+    username: "travel_junkie",
+    profilePic: "https://i.pravatar.cc/150?u=2",
+    postImage: "https://picsum.photos/id/1016/600/600",
+    likes: 852,
+    caption: "The mountains are calling. 🏔️",
+  },
+  {
+    id: 3,
+    username: "foodie_vibes",
+    profilePic: "https://i.pravatar.cc/150?u=3",
+    postImage: "https://picsum.photos/id/102/600/600",
+    likes: 1205,
+    caption: "Best ramen in the city! 🍜✨",
+  },
+  {
+    id: 4,
+    username: "pixel_art",
+    profilePic: "https://i.pravatar.cc/150?u=4",
+    postImage: "https://picsum.photos/id/1024/600/600",
+    likes: 95,
+    caption: "Macro photography is insane. 📸",
+  },
+  {
+    id: 5,
+    username: "fitness_edge",
+    profilePic: "https://i.pravatar.cc/150?u=5",
+    postImage: "https://picsum.photos/id/1070/600/600",
+    likes: 310,
+    caption: "Morning grind. No excuses. 💪",
+  }
+];
+
+export const STORIES = [
+  { id: 1, username: "your_story", img: "https://i.pravatar.cc/150?u=8" },
+  { id: 2, username: "dev_guy", img: "https://i.pravatar.cc/150?u=7" },
+  { id: 3, username: "js_wizard", img: "https://i.pravatar.cc/150?u=6" },
+  { id: 4, username: "ui_design", img: "https://i.pravatar.cc/150?u=9" },
+  { id: 5, username: "tech_news", img: "https://i.pravatar.cc/150?u=11" },
+  { id: 6, username: "coffee_art", img: "https://i.pravatar.cc/150?u=12" },
+  { id: 7, username: "nature_hub", img: "https://i.pravatar.cc/150?u=13" },
+];
